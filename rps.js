fd2cfea8abe993ec4e1c5622b3dd36f15dde72fd
StewieR <richardson.stewie@gmail.com>
2017-07-06 20:48:52 +0200

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,75 +1,74 @@
 $(document).ready(function() {
 
+$(".rock").click(function() {
 
-	$(".rock").click(function() {
+	
+	$(".paper").fadeOut().delay(1500).fadeIn();
+	$(".scissors").fadeOut().delay(1500).fadeIn();
 
+	$(this).animate({"left": "2.7em"}, 800).delay(850).animate({"left": "0.7em"}, 800);
 
-		$(".paper").fadeOut().delay(1500).fadeIn();
-		$(".scissors").fadeOut().delay(1500).fadeIn();
+			var computerPick = (Math.random() * (3 - 1) + 1).toFixed(0);
 
-		$(this).animate({"left": "2.7em"}, 800).delay(850).animate({"left": "0.7em"}, 800);
+		if (computerPick == 1) {
 
-				var computerPick = (Math.random() * (3 - 1) + 1).toFixed(0);
+			$(".paper2").fadeOut().delay(1500).fadeIn();
+			$(".scissors2").fadeOut().delay(1500).fadeIn();
 
-			if (computerPick == 1) {
+			$(".rock2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
-				$(".paper2").fadeOut().delay(1500).fadeIn();
-				$(".scissors2").fadeOut().delay(1500).fadeIn();
-
-				$(".rock2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
-
-					var rockTie = $("#midButton").html("2 Rocks, no winner") &&
-								$("#midButton").css({"left": "34.3em"});
+				var rockTie = $("#midButton").html("2 Rocks, no winner") &&
+					      $("#midButton").css({"left": "34.3em"});
 						
-						$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
-						$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
-						$(rockTie).fadeIn(300).delay(1900).fadeOut(300);
+				$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
+				$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
+				$(rockTie).fadeIn(300).delay(1900).fadeOut(300);
 
-			}
+		}
 
-			else if (computerPick == 2) {
+		else if (computerPick == 2) {
 
-				$(".rock2").fadeOut().delay(1500).fadeIn();
-				$(".scissors2").fadeOut().delay(1500).fadeIn();
+			$(".rock2").fadeOut().delay(1500).fadeIn();
+			$(".scissors2").fadeOut().delay(1500).fadeIn();
 
-				$(".paper2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
+			$(".paper2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
-					var rockLost = $("#midButton").html("Go and pick up new rocks") &&
-								 $("#midButton").css({"left": "32.6em"});
+				var rockLost = $("#midButton").html("Go and pick up new rocks") &&
+					       $("#midButton").css({"left": "32.6em"});
 						
-						$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
-						$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
-						$(rockLost).fadeIn(300).delay(1900).fadeOut(300);
+					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
+					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
+					$(rockLost).fadeIn(300).delay(1900).fadeOut(300);
 
-			}
+		}
 
-			else {
+		else {
 
-				$(".rock2").fadeOut().delay(1500).fadeIn();
-				$(".paper2").fadeOut().delay(1500).fadeIn();
+			$(".rock2").fadeOut().delay(1500).fadeIn();
+			$(".paper2").fadeOut().delay(1500).fadeIn();
 
-				$(".scissors2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
+			$(".scissors2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
-					var rockWon = $("#midButton").html("Sick rock bro!") &&
-								$("#midButton").css({"left": "35.2em"});
+				var rockWon = $("#midButton").html("Sick rock bro!") &&
+					      $("#midButton").css({"left": "35.2em"});
 						
-						$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
-						$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
-						$(rockWon).fadeIn(300).delay(1900).fadeOut(300);
+					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
+					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
+					$(rockWon).fadeIn(300).delay(1900).fadeOut(300);
 
-			}
+		}
 
-		});
+});
 
 
 $(".paper").click(function() {
 
 
-	$(".paper").animate({"left": "2.7em"}, 800).delay(850).animate({"left": "0.7em"}, 800);
-
 	$(".rock").fadeOut().delay(1500).fadeIn();
 	$(".scissors").fadeOut().delay(1500).fadeIn();
-
+	
+		$(this).animate({"left": "2.7em"}, 800).delay(850).animate({"left": "0.7em"}, 800);
+	
 			var computerPick = (Math.random() * (3 - 1) + 1).toFixed(0);
 					
 		if (computerPick == 1) {
@@ -80,7 +79,7 @@ $(".paper").click(function() {
 			$(".rock2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
 				var paperWon = $("#midButton").html("Rock dressed as paper") &&
-							 $("#midButton").css({"left": "33.7em"});
+					       $("#midButton").css({"left": "33.7em"});
 						
 					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
 					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
@@ -96,7 +95,7 @@ $(".paper").click(function() {
 			$(".paper2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
 				var paperTie = $("#midButton").html("Paper & Paper..? Nah") &&
-							$("#midButton").css({"left": "33.8em"});
+					       $("#midButton").css({"left": "33.8em"});
 						
 					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
 					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
@@ -112,7 +111,7 @@ $(".paper").click(function() {
 			$(".scissors2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
 				var paperLost = $("#midButton").html("Paper got pierced by scissors") &&
-								$("#midButton").css({"left": "31.9em"});
+						$("#midButton").css({"left": "31.9em"});
 
 					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
 					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
@@ -142,7 +141,7 @@ $(".scissors").click(function() {
 			$(".rock2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
 				var scissorsLost = $("#midButton").html("Buy yourself some new scissors") &&
-								$("#midButton").css({"left": "31.5em"});
+						   $("#midButton").css({"left": "31.5em"});
 						
 					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
 					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
@@ -158,7 +157,7 @@ $(".scissors").click(function() {
 			$(".paper2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
 				var scissorsWon = $("#midButton").html("Dat paper got cut in pieces") &&
-								$("#midButton").css({"left": "32.6em"});
+						  $("#midButton").css({"left": "32.6em"});
 						
 					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
 					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
@@ -174,7 +173,7 @@ $(".scissors").click(function() {
 			$(".scissors2").animate({"right": "2.7em"}, 800).delay(850).animate({"right": "0.7em"}, 800);
 
 				var scissorsTie = $("#midButton").html("RPS gone sexual") &&
-								$("#midButton").css({"left": "34.8em"});
+						  $("#midButton").css({"left": "34.8em"});
 						
 					$(".left-side-hr").animate({"left": "44.5em"}, 200).delay(2200).animate({"left": "46em"}, 200);
 					$(".right-side-hr").animate({"right": "44.5em"}, 200).delay(2200).animate({"right": "46em"}, 200);
